9b6082193ff4c41a31da33817a15881f759c72d6
Wissale23 <wz2923@ic.ac.uk>
2025-11-24 23:30:14 +0000

    Enhance DisplayImage with comments for clarity
    Added comments to clarify the purpose of the DisplayImage constructor and the for loop for displaying multiple images.

--- src/main/java/UIData/DisplayImage.java
+++ src/main/java/UIData/DisplayImage.java
@@ -7,8 +7,8 @@ import java.net.URL;
 
 public class DisplayImage extends JPanel {
 
-    public DisplayImage(String url){
-        JLabel label= new JLabel();
+    public DisplayImage(String url){ // String[] urls then you do for loop for everything, 
+        JLabel label= new JLabel(); //A JLabel is a Swing component that can display text or images. JLabel will have to have rows that = number of urls
         URL imageURL = null;
 
         try {
@@ -19,9 +19,13 @@ public class DisplayImage extends JPanel {
             System.out.println(e.getMessage());
         }
 
-        ImageIcon icon = new ImageIcon(imageURL);
+        ImageIcon icon = new ImageIcon(imageURL); //ImageIcon is a Swing object that can store and display images in components like JLabel.
         label.setIcon(icon);
         add(label);
 
     }
+}
+
+for (String url : mri1.getMRIURLs()) { // mri1.getMRIURLs.size() returns number of urls
+    System.out.println(url);
 }
