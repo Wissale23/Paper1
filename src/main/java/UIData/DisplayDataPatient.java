7352c7a12c4b7fe97b603947e928567975d95381
Wissale23 <wz2923@ic.ac.uk>
2025-11-24 23:46:11 +0000

    Modify JLabel text to display patient details
    Updated the text of the JLabel to include patient age and name.

--- src/main/java/UIData/DisplayDataPatient.java
+++ src/main/java/UIData/DisplayDataPatient.java
@@ -8,7 +8,8 @@ import javax.swing.*;
 public class DisplayDataPatient extends JPanel{
     public DisplayDataPatient(Diagnosis diagnosis1){
         JLabel textPatient = new JLabel("My text");
-        textPatient.setText("<html>"+"Name: "+diagnosis1.getName()+"<br>"+"Age: "+diagnosis1.getAge()+"</html>");
+        textPatient.setText("<html>"+"Name: "+diagnosis1.getName()+"<br>"+"Age: "+diagnosis1.getAge()+"</html>");// here we can have diagnosis1.getExamData since diagnosis has o=to extend to abstract
+        //maybe add new label then?
         add(textPatient);
     }
 
